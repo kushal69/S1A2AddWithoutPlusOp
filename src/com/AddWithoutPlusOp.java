4c76a0112ac556080755d8804a62e2b42e6cc627
kushal <kushalmys369@gmail.com>
2017-08-15 13:40:39 +0530

    Initial Commit - ACADGild Session 1 Assignment 2

---
+++ src/com/AddWithoutPlusOp.java
@@ -0,0 +1,27 @@
+package com;
+
+public class AddWithoutPlusOp {
+
+	public static int add(int a, int b) {
+		return a - ~b - 1;
+	}
+
+	public static void main(String[] args) {
+
+		int result = 0;
+
+		result = add(100, -235);
+		System.out.println("Addition Result --------------------> " + result);
+
+		result = add(100, 235);
+		System.out.println("Addition Result --------------------> " + result);
+
+		result = add(-100, -235);
+		System.out.println("Addition Result --------------------> " + result);
+
+		result = add(0, -235);
+		System.out.println("Addition Result --------------------> " + result);
+
+	}
+
+}
